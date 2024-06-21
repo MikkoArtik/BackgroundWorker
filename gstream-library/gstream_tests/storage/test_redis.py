cad9e94f313117ce12a581b10e65a865a396fe11
Roostique <mannapovr@sigma-geophys.com>
2024-06-21 12:44:42 +0300

    Add redis tests

--- gstream-library/gstream_tests/storage/test_redis.py
+++ gstream-library/gstream_tests/storage/test_redis.py
@@ -1,8 +1,9 @@
 import datetime
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, AsyncMock, PropertyMock
 import pytest
 from hamcrest import assert_that, equal_to
 from gstream.storage.redis import Storage, DATETIME_FORMAT, format_message
+from redis.asyncio import ConnectionPool, Redis
 
 
 class TestFormatMessage:
@@ -36,4 +37,63 @@ class TestStorage:
         assert_that(
             actual_or_assertion=obj._Storage__adapter,
             matcher=equal_to(adapter)
+        )
+
+    @pytest.mark.positive
+    @pytest.mark.parametrize(
+        ['src_dict', 'expected_value'],
+        [
+            ({'key_': 1}, {'key_': 1}),
+            ({'key_': {'key2': 2}}, {'key_:key2': 2}),
+            ({'key_': {'key2': {'key3': 3}}}, {'key_:key2:key3': 3}),
+            ({'key_': True}, {'key_': 1}),
+            ({'key_': False}, {'key_': 0})
+         ]
+    )
+    def test_create_flatten_dict_positive(
+            self,
+            src_dict: dict,
+            expected_value: dict
+    ):
+        obj = Storage(pool=Mock(), key_expiration='test')
+
+        assert_that(
+            actual_or_assertion=obj._Storage__create_flatten_dict(
+                src_dict=src_dict
+            ),
+            matcher=equal_to(expected_value)
+        )
+
+    @pytest.mark.positive
+    def test_adapter_positive(self):
+        adapter = 'adapter'
+        obj = Storage(pool=Mock(), key_expiration='test')
+        obj._Storage__adapter = adapter
+
+        assert_that(
+            actual_or_assertion=obj.adapter,
+            matcher=equal_to(adapter)
+        )
+
+    @pytest.mark.positive
+    @pytest.mark.asyncio
+    @patch.object(Storage, 'adapter', new_callable=PropertyMock)
+    @patch.object(Storage, '_Storage__create_flatten_dict')
+    async def test_add_dict_positive(
+            self,
+            mock_create_dict: Mock,
+            mock_adapter: Mock
+    ):
+        flatten_dict = {1: 1}
+        mock_create_dict.return_value = flatten_dict
+        mset = AsyncMock()
+        mset.return_value = None
+        mock_adapter.return_value = mset
+        await Storage(
+            pool=AsyncMock(),
+            key_expiration='test'
+        )._Storage__add_dict(value='q')
+
+        mock_adapter.return_value.mset.assert_called_once_with(
+            mapping=flatten_dict
         )
